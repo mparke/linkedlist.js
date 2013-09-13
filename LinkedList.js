05eb634aa38ae0a68b02e0e9f4f22c7018ea8cb4
Matthew Parke <mattp@aweber.com>
2013-09-13 10:53:30 -0400

    LinkedList: Add aliases and each

--- LinkedList.js
+++ LinkedList.js
@@ -2,182 +2,241 @@ var LinkedList = (function(){
 
 	function LinkedList(){
 		//initialize end buffer nodes
-        this.start = new ListNode();
-        this.end = new ListNode();
+    this.start = new ListNode();
+    this.end = new ListNode();
 
-        //initialize node pointers
-        this.start.next = this.end;
-        this.start.prev = null;
+    //initialize node pointers
+    this.start.next = this.end;
+    this.start.prev = null;
 
-        this.end.prev = this.start;
-        this.end.next = null;
+    this.end.prev = this.start;
+    this.end.next = null;
 
-        //initialize counters
-        this.idCounter = 0;
-        this.numNodes = 0;
+    //initialize counters
+    this._idCounter = 0;
+    this._numNodes = 0;
 	}
 
 	LinkedList.prototype = {
 
-		/**
-	    *   Alias for addLast
-	    *   @param {object} data
-	    */
-	    add: function (data) {
-	        this.addLast(data);
-	    },
+    /**
+    *   Adds data wrapped in a Node object to the end of the linked list
+    *   @param {object} data
+    */
+    addLast: function (data) {
+      var node = new ListNode(this._idCounter, data);
 
-	    /**
-	    *   Adds data wrapped in a Node object to the end of the linked list
-	    *   @param {object} data
-	    */
-	    addLast: function (data) {
-	        var node = new ListNode(this.idCounter, data);
+      last = this.end;
 
-	        last = this.end;
+      this.insertBefore(last, node);
 
-	        this.insertBefore(last, node);
+      ++this._idCounter;
+      ++this._numNodes;
+    },
 
-	        ++this.idCounter;
-	        ++this.numNodes;
-	    },
+    /**
+    *   Inserts a node before another node in the linked list
+    *   @param {Node} toInsertBefore
+    *   @param {Node} node
+    */
+    insertBefore: function (toInsertBefore, node) {
+      node.next = toInsertBefore;
+      node.prev = toInsertBefore.prev;
 
-	    /**
-	    *   Inserts a node before another node in the linked list
-	    *   @param {Node} toInsertBefore
-	    *   @param {Node} node
-	    */ 
-	    insertBefore: function (toInsertBefore, node) {
-	        node.next = toInsertBefore;
-	        node.prev = toInsertBefore.prev;
+      toInsertBefore.prev.next = node;
+      toInsertBefore.prev = node;
+    },
 
-	        toInsertBefore.prev.next = node;
-	        toInsertBefore.prev = node;
-	    },
+    /**
+    *   Gets and returns the first node in the linked list or null
+    *   @return {Node/null}
+    */
+    getFirst: function () {
+      if (this._numNodes === 0) {
+          return null;
+      } else {
+          return this.start.next;
+      }
+    },
 
-	    /**
-	    *   Gets and returns the first node in the linked list or null
-	    *   @return {Node/null}
-	    */
-	    getFirst: function () {
-	        if (this.numNodes === 0) {
-	            return null;
-	        } else {
-	            return this.start.next;
-	        }
-	    },
+    /**
+    *   Gets and returns the last node in the linked list or null
+    *   @return {Node/null}
+    */
+    getLast: function () {
+      if (this._numNodes === 0) {
+          return null;
+      } else {
+          return this.end.prev;
+      }
+    },
 
-	    /**
-	    *   Gets and returns the last node in the linked list or null
-	    *   @return {Node/null}
-	    */
-	    getLast: function () {
-	        if (this.numNodes === 0) {
-	            return null;
-	        } else {
-	            return this.end.prev;
-	        }
-	    },
+    /**
+    *   (Internal) Gets and returns the node at the specified index starting from the first in the linked list
+    *   Use getAt instead of this function
+    *   @param {number} index
+    */
+    getFromFirst: function (index) {
+      var count = 0,
+          temp = this.start.next;
 
-	    /**
-	    *   (Internal) Gets and returns the node at the specified index starting from the first in the linked list
-	    *   Use getAt instead of this function
-	    *   @param {number} index
-	    */
-	    getFromFirst: function (index) {
-	        var count = 0,
-	            temp = this.start.next;
+      if(index >= 0){
+          while (count < index && temp !== null) {
+              temp = temp.next;
+              ++count;
+          }
+      }else{
+          temp = null;
+      }
 
-	        if(index >= 0){
-	            while (count < index && temp !== null) {
-	                temp = temp.next;
-	                ++count;
-	            }
-	        }else{
-	            temp = null;
-	        }
-	    
-	        if(temp === null){
-	            throw 'Index out of bounds.';
-	        }
+      if(temp === null){
+          throw 'Index out of bounds.';
+      }
 
-	        return temp;
-	    },
+      return temp;
+    },
 
-	    /**
-	    *   Gets and returns the Node at the specified index in the linked list
-	    *   @param {number} index
-	    */
-	    getAt: function (index) {
-	        var count = 0,
-	            temp = null;
+    /**
+    *   Gets and returns the Node at the specified index in the linked list
+    *   @param {number} index
+    */
+    get: function (index) {
+      var count = 0,
+          temp = null;
 
-	        if (index === 0) {
-	            temp = this.getFirst();
-	        } else if (index === this.numNodes - 1) {
-	            temp = this.getLast();
-	        } else {
-	            temp = this.getFromFirst(index);
-	        }
+      if (index === 0) {
+          temp = this.getFirst();
+      } else if (index === this._numNodes - 1) {
+          temp = this.getLast();
+      } else {
+          temp = this.getFromFirst(index);
+      }
 
-	        return temp;
-	    },
+      return temp;
+    },
 
-	    /**
-	    *   Gets and returns the size of the linked list
-	    *   @return {number}
-	    */
-	    size: function () {
-	        return this.numNodes;
-	    },
+    /**
+    *		Inserts a node at the given index, shifting the list forward
+    *   @param {number} index
+    *		@param {object} data
+    */
+    insert: function(index, data) {
+    	var toInsertBefore = this.get(index),
+    		newNode = new ListNode(this._idCounter, data);
 
-	    /**
-	    *   Removes and returns node from the linked list by rearranging pointers
-	    *   @param {Node} node
-	    *   @return {Node}
-	    */
-	    remove: function (node) {
-	        node.prev.next = node.next;
-	        node.next.prev = node.prev;
+    	this.insertBefore(toInsertBefore, newNode);
+    },
 
-	        --this.numNodes;
+    /**
+    *   Gets and returns the size of the linked list
+    *   @return {number}
+    */
+    size: function () {
+      return this._numNodes;
+    },
 
-	        return node;
-	    },
+    /**
+    *   Removes and returns node from the linked list by rearranging pointers
+    *   @param {Node} node
+    *   @return {Node}
+    */
+    remove: function (node) {
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
 
-	    /**
-	    *   Removes and returns the first node in the linked list if it exists, otherwise returns null
-	    *   @return {Node/null}
-	    */
-	    removeFirst: function () {
-	        var temp = null;
+      --this._numNodes;
 
-	        if (this.numNodes > 0) {
-	            temp = this.remove(this.start.next);
-	        }
+      return node;
+    },
 
-	        return temp;
-	    },
+    /**
+    *   Removes and returns the first node in the linked list if it exists, otherwise returns null
+    *   @return {Node/null}
+    */
+    removeFirst: function () {
+      var temp = null;
 
-	    /**
-	    *   Removes and returns the last node in the linked list if it exists, otherwise returns null
-	    *   @return {Node/null}
-	    */
-	    removeLast: function () {
-	        var temp = null;
+      if (this._numNodes > 0) {
+        temp = this.remove(this.start.next);
+      }
 
-	        if (this.numNodes > 0) {
-	            temp = this.remove(this.end.prev);
-	        }
+      return temp;
+    },
 
-	        return temp;
-	    },
+    /**
+    *   Removes and returns the last node in the linked list if it exists, otherwise returns null
+    *   @return {Node/null}
+    */
+    removeLast: function () {
+      var temp = null;
 
-	    removeAll: function(){
-	        this.start.next = this.end;
-	        this.end.prev = this.start;
-	        this.numNodes = 0;
-	    }
+      if (this._numNodes > 0) {
+        temp = this.remove(this.end.prev);
+      }
+
+      return temp;
+    },
+
+    removeAll: function(){
+      this.start.next = this.end;
+      this.end.prev = this.start;
+      this._numNodes = 0;
+    },
+
+    /**
+    *		Iterates the list calling the given fn for each node
+    *		@param {function} fn
+    */
+    each: function(fn) {
+    	var temp = this.getFirst();
+
+    	if(temp) {
+    		fn(temp);
+
+    		while(temp.hasNext()) {
+    			temp = temp.next;
+    			fn(temp);
+	    	}
+    	}
+    },
+
+    /**
+    *   Alias for addLast
+    *   @param {object} data
+    */
+    add: function (data) {
+      this.addLast(data);
+    },
+
+    /**
+    *		Alias for addLast
+    *		@param {object} data
+    */
+    push: function(data) {
+    	this.addLast(data);
+    },
+
+    /**
+    *		Performs insertBefore on the first node
+    *		@param {object} data
+    */
+    unshift: function(data) {
+    	this.insertBefore(data, this.getFirst());
+    },
+
+    /**
+		*		Alias for removeLast
+		*/
+    pop: function() {
+    	return this.removeLast();
+    },
+
+    /**
+    *		Alias for removeFirst()
+    */
+    shift: function() {
+    	return this.removeFirst();
+    }
 	};
 
 	return LinkedList;
--- test/LinkedList/LinkedList_Test.js
+++ test/LinkedList/LinkedList_Test.js
@@ -1,15 +1,16 @@
 describe('LinkedList', function(){
 
 	it('can be instantiated with defaults.', function(){
+
 		var list = new LinkedList();
 
 		expect(list).toBeDefined();
-		expect(list.start.getId()).toBeNull();
-		expect(list.end.getId()).toBeNull();
+		expect(list.start.id()).toBeNull();
+		expect(list.end.id()).toBeNull();
 		expect(list.start.prev).toBeNull();
 		expect(list.end.next).toBeNull();
-		expect(list.idCounter).toBe(0);
-		expect(list.numNodes).toBe(0);
+		expect(list._idCounter).toBe(0);
+		expect(list._numNodes).toBe(0);
 	});
 
 	it('LinkedList.insertBefore: can insert a node before another node.', function(){
@@ -20,43 +21,43 @@ describe('LinkedList', function(){
 
 		list.insertBefore(list.end, firstNode);
 
-		expect(list.start.next.getId()).toBe(1);
-		expect(list.start.next.getData().val).toBe(10);
-		expect(list.end.prev.getId()).toBe(1);
-		expect(list.end.prev.getData().val).toBe(10);
+		expect(list.start.next.id()).toBe(1);
+		expect(list.start.next.data().val).toBe(10);
+		expect(list.end.prev.id()).toBe(1);
+		expect(list.end.prev.data().val).toBe(10);
 
 		var secondNode = new ListNode(2, {val:20});
 
 		list.insertBefore(firstNode, secondNode);
 
-		expect(list.start.next.getId()).toBe(2);
-		expect(list.start.next.getData().val).toBe(20);
+		expect(list.start.next.id()).toBe(2);
+		expect(list.start.next.data().val).toBe(20);
 
-		expect(list.start.next.next.getId()).toBe(1);
-		expect(list.start.next.next.getData().val).toBe(10);
+		expect(list.start.next.next.id()).toBe(1);
+		expect(list.start.next.next.data().val).toBe(10);
 
-		expect(list.end.prev.getId()).toBe(1);
-		expect(list.end.prev.getData().val).toBe(10);
-	
-		expect(list.end.prev.prev.getId()).toBe(2);
-		expect(list.end.prev.prev.getData().val).toBe(20);
-	
+		expect(list.end.prev.id()).toBe(1);
+		expect(list.end.prev.data().val).toBe(10);
+
+		expect(list.end.prev.prev.id()).toBe(2);
+		expect(list.end.prev.prev.data().val).toBe(20);
+
 	});
 
 	it('LinkedList.addLast: can add to the end of the list.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
-		
+
 		list.addLast({val:10});
 		list.addLast({val:20});
 		list.addLast({val:30});
 
-		expect(list.start.next.getData().val).toBe(10);
-		expect(list.start.next.next.getData().val).toBe(20);
-		expect(list.start.next.next.next.getData().val).toBe(30);
-		expect(list.end.prev.getData().val).toBe(30);
-		expect(list.end.prev.prev.getData().val).toBe(20);
-		expect(list.end.prev.prev.prev.getData().val).toBe(10);
+		expect(list.start.next.data().val).toBe(10);
+		expect(list.start.next.next.data().val).toBe(20);
+		expect(list.start.next.next.next.data().val).toBe(30);
+		expect(list.end.prev.data().val).toBe(30);
+		expect(list.end.prev.prev.data().val).toBe(20);
+		expect(list.end.prev.prev.prev.data().val).toBe(10);
 
 	});
 
@@ -68,12 +69,12 @@ describe('LinkedList', function(){
 		list.add({val:20});
 		list.add({val:30});
 
-		expect(list.start.next.getData().val).toBe(10);
-		expect(list.start.next.next.getData().val).toBe(20);
-		expect(list.start.next.next.next.getData().val).toBe(30);
-		expect(list.end.prev.getData().val).toBe(30);
-		expect(list.end.prev.prev.getData().val).toBe(20);
-		expect(list.end.prev.prev.prev.getData().val).toBe(10);
+		expect(list.start.next.data().val).toBe(10);
+		expect(list.start.next.next.data().val).toBe(20);
+		expect(list.start.next.next.next.data().val).toBe(30);
+		expect(list.end.prev.data().val).toBe(30);
+		expect(list.end.prev.prev.data().val).toBe(20);
+		expect(list.end.prev.prev.prev.data().val).toBe(10);
 	});
 
 	it('LinkedList.getFirst: can get and return the first node.', function(){
@@ -86,20 +87,20 @@ describe('LinkedList', function(){
 		list.add({val:20});
 		list.add({val:30});
 
-		expect(list.getFirst().getData().val).toBe(10);
+		expect(list.getFirst().data().val).toBe(10);
 	});
 
 	it('LinkedList.getLast: can get and return the last node.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
-		
+
 		expect(list.getLast()).toBeNull();
 
 		list.add({val:10});
 		list.add({val:20});
 		list.add({val:30});
 
-		expect(list.getLast().getData().val).toBe(30);
+		expect(list.getLast().data().val).toBe(30);
 	});
 
 	it('LinkedList.size: can get and return the size of the list.', function(){
@@ -131,22 +132,22 @@ describe('LinkedList', function(){
 		list.add({val:90});
 
 		expect(list.getFromFirst(0)).toBeDefined();
-		expect(list.getFromFirst(0).getData().val).toBe(10);
+		expect(list.getFromFirst(0).data().val).toBe(10);
 
 		expect(list.getFromFirst(8)).toBeDefined();
-		expect(list.getFromFirst(8).getData().val).toBe(90);
+		expect(list.getFromFirst(8).data().val).toBe(90);
 
 		expect(list.getFromFirst(5)).toBeDefined();
-		expect(list.getFromFirst(5).getData().val).toBe(60);
+		expect(list.getFromFirst(5).data().val).toBe(60);
 
 		expect(list.getFromFirst(2)).toBeDefined();
-		expect(list.getFromFirst(2).getData().val).toBe(30);
+		expect(list.getFromFirst(2).data().val).toBe(30);
 
 		expect(function(){list.getFromFirst(100);}).toThrow('Index out of bounds.');
 		expect(function(){list.getFromFirst(-100);}).toThrow('Index out of bounds.');
 	});
 
-	it('LinkedList.getAt: can get a node at an index, optimizing for first and last indices.', function(){
+	it('LinkedList.get: can get a node at an index, optimizing for first and last indices.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
 		expect(list.size()).toBe(0);
@@ -161,20 +162,20 @@ describe('LinkedList', function(){
 		list.add({val:80});
 		list.add({val:90});
 
-		expect(list.getAt(0)).toBeDefined();
-		expect(list.getAt(0).getData().val).toBe(10);
+		expect(list.get(0)).toBeDefined();
+		expect(list.get(0).data().val).toBe(10);
 
-		expect(list.getAt(8)).toBeDefined();
-		expect(list.getAt(8).getData().val).toBe(90);
+		expect(list.get(8)).toBeDefined();
+		expect(list.get(8).data().val).toBe(90);
 
-		expect(list.getAt(5)).toBeDefined();
-		expect(list.getAt(5).getData().val).toBe(60);
+		expect(list.get(5)).toBeDefined();
+		expect(list.get(5).data().val).toBe(60);
 
-		expect(list.getAt(2)).toBeDefined();
-		expect(list.getAt(2).getData().val).toBe(30);
+		expect(list.get(2)).toBeDefined();
+		expect(list.get(2).data().val).toBe(30);
 
-		expect(function(){list.getAt(100);}).toThrow('Index out of bounds.');
-		expect(function(){list.getAt(-100);}).toThrow('Index out of bounds.');
+		expect(function(){list.get(100);}).toThrow('Index out of bounds.');
+		expect(function(){list.get(-100);}).toThrow('Index out of bounds.');
 	});
 
 	it('LinkedList.remove: can remove a node from the list.', function(){
@@ -190,17 +191,17 @@ describe('LinkedList', function(){
 
 		var firstNode = list.remove(list.getFirst());
 		expect(firstNode).toBeDefined();
-		expect(firstNode.getData().val).toBe(10);
+		expect(firstNode.data().val).toBe(10);
 		expect(list.size()).toBe(2);
 
 		var secondNode = list.remove(list.getFirst());
 		expect(secondNode).toBeDefined();
-		expect(secondNode.getData().val).toBe(20);
+		expect(secondNode.data().val).toBe(20);
 		expect(list.size()).toBe(1);
 
 		var thirdNode = list.remove(list.getLast());
 		expect(thirdNode).toBeDefined();
-		expect(thirdNode.getData().val).toBe(30);
+		expect(thirdNode.data().val).toBe(30);
 		expect(list.size()).toBe(0);
 	});
 
@@ -217,17 +218,17 @@ describe('LinkedList', function(){
 
 		var firstNode = list.removeFirst();
 		expect(firstNode).toBeDefined();
-		expect(firstNode.getData().val).toBe(10);
+		expect(firstNode.data().val).toBe(10);
 		expect(list.size()).toBe(2);
 
 		var secondNode = list.removeFirst();
 		expect(secondNode).toBeDefined();
-		expect(secondNode.getData().val).toBe(20);
+		expect(secondNode.data().val).toBe(20);
 		expect(list.size()).toBe(1);
 
 		var thirdNode = list.removeFirst();
 		expect(thirdNode).toBeDefined();
-		expect(thirdNode.getData().val).toBe(30);
+		expect(thirdNode.data().val).toBe(30);
 		expect(list.size()).toBe(0);
 	});
 
@@ -244,17 +245,17 @@ describe('LinkedList', function(){
 
 		var firstNode = list.removeLast();
 		expect(firstNode).toBeDefined();
-		expect(firstNode.getData().val).toBe(30);
+		expect(firstNode.data().val).toBe(30);
 		expect(list.size()).toBe(2);
 
 		var secondNode = list.removeLast();
 		expect(secondNode).toBeDefined();
-		expect(secondNode.getData().val).toBe(20);
+		expect(secondNode.data().val).toBe(20);
 		expect(list.size()).toBe(1);
-	
+
 		var thirdNode = list.removeLast();
 		expect(thirdNode).toBeDefined();
-		expect(thirdNode.getData().val).toBe(10);
+		expect(thirdNode.data().val).toBe(10);
 		expect(list.size()).toBe(0);
 	});
 
@@ -272,6 +273,21 @@ describe('LinkedList', function(){
 		list.removeAll();
 
 		expect(list.size()).toBe(0);
+	});
+
+	it('LinkedList.each: can iterate the list with each', function() {
+		var list = new LinkedList();
+
+		list.add({val:10});
+		list.add({val:20});
+		list.add({val:30});
+
+		var spy = jasmine.createSpy();
+
+		list.each(spy);
+
+		expect(spy).toHaveBeenCalled();
+		expect(spy.callCount).toEqual(3);
 	});
 
 });
