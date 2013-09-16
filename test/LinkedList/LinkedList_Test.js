942f98b4f1ff9bfab22d9529e2a16440aac75727
Matthew Parke <mattp@aweber.com>
2013-09-16 11:29:16 -0400

    LinkedListTest: Update tests for find, map, and aliases

--- test/LinkedList/LinkedList_Test.js
+++ test/LinkedList/LinkedList_Test.js
@@ -1,7 +1,6 @@
 describe('LinkedList', function(){
 
 	it('can be instantiated with defaults.', function(){
-
 		var list = new LinkedList();
 
 		expect(list).toBeDefined();
@@ -13,38 +12,34 @@ describe('LinkedList', function(){
 		expect(list._numNodes).toBe(0);
 	});
 
-	it('LinkedList.insertBefore: can insert a node before another node.', function(){
+	it('insertBefore: can insert a node before another node.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
 
-		var firstNode = new ListNode(1, {val:10});
+		list.insertBefore(list.end, {val:10});
 
-		list.insertBefore(list.end, firstNode);
+		expect(list.start.next.id()).toBe(0);
+		expect(list.start.next.data().val).toBe(10);
+		expect(list.end.prev.id()).toBe(0);
+		expect(list.end.prev.data().val).toBe(10);
+
+		list.insertBefore(list.start.next, {val:20});
 
 		expect(list.start.next.id()).toBe(1);
-		expect(list.start.next.data().val).toBe(10);
-		expect(list.end.prev.id()).toBe(1);
-		expect(list.end.prev.data().val).toBe(10);
-
-		var secondNode = new ListNode(2, {val:20});
-
-		list.insertBefore(firstNode, secondNode);
-
-		expect(list.start.next.id()).toBe(2);
 		expect(list.start.next.data().val).toBe(20);
 
-		expect(list.start.next.next.id()).toBe(1);
+		expect(list.start.next.next.id()).toBe(0);
 		expect(list.start.next.next.data().val).toBe(10);
 
-		expect(list.end.prev.id()).toBe(1);
+		expect(list.end.prev.id()).toBe(0);
 		expect(list.end.prev.data().val).toBe(10);
 
-		expect(list.end.prev.prev.id()).toBe(2);
+		expect(list.end.prev.prev.id()).toBe(1);
 		expect(list.end.prev.prev.data().val).toBe(20);
 
 	});
 
-	it('LinkedList.addLast: can add to the end of the list.', function(){
+	it('addLast: can add to the end of the list.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
 
@@ -61,7 +56,7 @@ describe('LinkedList', function(){
 
 	});
 
-	it('LinkedList.add: can add a data object to the list.', function(){
+	it('add: can add a data object to the list.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
 
@@ -77,7 +72,7 @@ describe('LinkedList', function(){
 		expect(list.end.prev.prev.prev.data().val).toBe(10);
 	});
 
-	it('LinkedList.getFirst: can get and return the first node.', function(){
+	it('getFirst: can get and return the first node.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
 
@@ -90,7 +85,7 @@ describe('LinkedList', function(){
 		expect(list.getFirst().data().val).toBe(10);
 	});
 
-	it('LinkedList.getLast: can get and return the last node.', function(){
+	it('getLast: can get and return the last node.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
 
@@ -103,7 +98,7 @@ describe('LinkedList', function(){
 		expect(list.getLast().data().val).toBe(30);
 	});
 
-	it('LinkedList.size: can get and return the size of the list.', function(){
+	it('size: can get and return the size of the list.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
 		expect(list.size()).toBe(0);
@@ -116,7 +111,7 @@ describe('LinkedList', function(){
 		expect(list.size()).toBe(3);
 	});
 
-	it('LinkedList.getFromFirst: can get a node at an index, starting the search from the first node.', function(){
+	it('getFromFirst: can get a node at an index, starting the search from the first node.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
 		expect(list.size()).toBe(0);
@@ -147,7 +142,7 @@ describe('LinkedList', function(){
 		expect(function(){list.getFromFirst(-100);}).toThrow('Index out of bounds.');
 	});
 
-	it('LinkedList.get: can get a node at an index, optimizing for first and last indices.', function(){
+	it('get: can get a node at an index, optimizing for first and last indices.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
 		expect(list.size()).toBe(0);
@@ -178,7 +173,7 @@ describe('LinkedList', function(){
 		expect(function(){list.get(-100);}).toThrow('Index out of bounds.');
 	});
 
-	it('LinkedList.remove: can remove a node from the list.', function(){
+	it('remove: can remove a node from the list.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
 		expect(list.size()).toBe(0);
@@ -205,7 +200,7 @@ describe('LinkedList', function(){
 		expect(list.size()).toBe(0);
 	});
 
-	it('LinkedList.removeFirst: can remove the first node from the list.', function(){
+	it('removeFirst: can remove the first node from the list.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
 		expect(list.size()).toBe(0);
@@ -232,7 +227,7 @@ describe('LinkedList', function(){
 		expect(list.size()).toBe(0);
 	});
 
-	it('LinkedList.removeLast: can remove the last node from the list.', function(){
+	it('removeLast: can remove the last node from the list.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
 		expect(list.size()).toBe(0);
@@ -259,7 +254,7 @@ describe('LinkedList', function(){
 		expect(list.size()).toBe(0);
 	});
 
-	it('LinkedList.removeAll: can remove all nodes from the list.', function(){
+	it('removeAll: can remove all nodes from the list.', function(){
 		var list = new LinkedList();
 		expect(list).toBeDefined();
 		expect(list.size()).toBe(0);
@@ -268,14 +263,16 @@ describe('LinkedList', function(){
 		list.add({val:20});
 		list.add({val:30});
 
-		expect(list.size()).toBe(3);
+		expect(list.size()).toEqual(3);
 
 		list.removeAll();
 
-		expect(list.size()).toBe(0);
+		expect(list.size()).toEqual(0);
+		expect(list._numNodes).toEqual(0);
+		expect(list._idCounter).toEqual(0);
 	});
 
-	it('LinkedList.each: can iterate the list with each', function() {
+	it('each: can iterate the list with each', function() {
 		var list = new LinkedList();
 
 		list.add({val:10});
@@ -290,4 +287,174 @@ describe('LinkedList', function(){
 		expect(spy.callCount).toEqual(3);
 	});
 
+	it('find: can find a node in the list using iterator', function() {
+		var list = new LinkedList();
+
+		list.add({val:10});
+		list.add({val:20});
+		list.add({val:30});
+
+		var spy = jasmine.createSpy();
+		var found = list.find(spy);
+
+		expect(spy).toHaveBeenCalled();
+		expect(spy.callCount).toEqual(3);
+		expect(found).toBe(null);
+
+		found = list.find(function(node) {
+			return node.data().val === 10;
+		});
+		expect(found).toBeDefined();
+		expect(found.id()).toEqual(0);
+		expect(found.data().val).toEqual(10);
+
+		found = list.find(function(node) {
+			return node.data().val === 20;
+		});
+		expect(found).toBeDefined();
+		expect(found.id()).toEqual(1);
+		expect(found.data().val).toEqual(20);
+
+		found = list.find(function(node) {
+			return node.data().val === 30;
+		});
+		expect(found).toBeDefined();
+		expect(found.id()).toEqual(2);
+		expect(found.data().val).toEqual(30);
+	});
+
+	it('map: can map nodes to an array that pass an iterator test', function() {
+		var list = new LinkedList();
+
+		list.add({val:10});
+		list.add({val:20});
+		list.add({val:30});
+
+		var spy = jasmine.createSpy();
+		var results = list.map(spy);
+
+		expect(spy).toHaveBeenCalled();
+		expect(spy.callCount).toEqual(3)
+		expect(results).toBeDefined();
+		expect(results.length).toEqual(0);
+
+		results = list.map(function(node) {
+			return node.data().val === 10;
+		});
+		expect(results).toBeDefined();
+		expect(results.length).toEqual(1);
+		expect(results[0].data().val).toEqual(10)
+
+		results = list.map(function(node) {
+			return node.data().val === 20;
+		});
+		expect(results).toBeDefined();
+		expect(results.length).toEqual(1);
+		expect(results[0].data().val).toEqual(20)
+
+		results = list.map(function(node) {
+			return node.data().val === 30;
+		});
+		expect(results).toBeDefined();
+		expect(results.length).toEqual(1);
+		expect(results[0].data().val).toEqual(30)
+
+		results = list.map(function(node) {
+			return node;
+		});
+		expect(results).toBeDefined();
+		expect(results.length).toEqual(3);
+		expect(results[0].data().val).toEqual(10);
+		expect(results[1].data().val).toEqual(20);
+		expect(results[2].data().val).toEqual(30);
+	});
+
+	it('push: supports push, an alias for addLast', function() {
+		var list = new LinkedList();
+
+		spyOn(list, 'addLast').andCallThrough();
+
+		list.push({val:10});
+		expect(list.addLast).toHaveBeenCalledWith({val:10});
+		list.push({val:20});
+		expect(list.addLast).toHaveBeenCalledWith({val:20});
+		list.push({val:30});
+		expect(list.addLast).toHaveBeenCalledWith({val:30});
+
+		expect(list.size()).toEqual(3);
+	});
+
+	it('unshift: supports unshift, an alias for insertBefore the first node', function() {
+		var list = new LinkedList();
+
+		spyOn(list, 'insertBefore').andCallThrough();
+
+		list.unshift({val:10});
+		expect(list.insertBefore).toHaveBeenCalledWith(list.end, {val:10});
+		expect(list.start.next).toBeDefined();
+		expect(list.start.next.data().val).toEqual(10);
+		expect(list.size()).toEqual(1);
+
+		list.unshift({val:20});
+		expect(list.start.next).toBeDefined();
+		expect(list.start.next.data().val).toEqual(20);
+		expect(list.size()).toEqual(2);
+
+		list.unshift({val:30});
+		expect(list.start.next).toBeDefined();
+		expect(list.start.next.data().val).toEqual(30);
+		expect(list.size()).toEqual(3);
+	});
+
+	it('pop: supports pop, an alias for removeLast', function() {
+		var list = new LinkedList();
+		list.add({val:10});
+		list.add({val:20});
+		list.add({val:30});
+
+		spyOn(list, 'removeLast').andCallThrough();
+
+		list.pop();
+		expect(list.removeLast).toHaveBeenCalled();
+		expect(list.removeLast.callCount).toEqual(1);
+		expect(list.size()).toEqual(2);
+		expect(list.end.prev.data().val).toEqual(20);
+
+		list.pop();
+		expect(list.removeLast).toHaveBeenCalled();
+		expect(list.removeLast.callCount).toEqual(2);
+		expect(list.size()).toEqual(1);
+		expect(list.end.prev.data().val).toEqual(10);
+
+		list.pop();
+		expect(list.removeLast).toHaveBeenCalled();
+		expect(list.removeLast.callCount).toEqual(3);
+		expect(list.size()).toEqual(0);
+	});
+
+	it('shift: supports shift, an alias for removeFirst', function() {
+		var list = new LinkedList();
+		list.add({val:10});
+		list.add({val:20});
+		list.add({val:30});
+
+		spyOn(list, 'removeFirst').andCallThrough();
+
+		list.shift();
+		expect(list.removeFirst).toHaveBeenCalled();
+		expect(list.removeFirst.callCount).toEqual(1);
+		expect(list.size()).toEqual(2);
+		expect(list.start.next.data().val).toEqual(20);
+
+		list.shift();
+		expect(list.removeFirst).toHaveBeenCalled();
+		expect(list.removeFirst.callCount).toEqual(2);
+		expect(list.size()).toEqual(1);
+		expect(list.start.next.data().val).toEqual(30);
+
+		list.shift();
+		expect(list.removeFirst).toHaveBeenCalled();
+		expect(list.removeFirst.callCount).toEqual(3);
+		expect(list.size()).toEqual(0);
+	});
 });
